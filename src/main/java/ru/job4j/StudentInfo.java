d8092d52ad9e29548df394a87bb31b7698215b13
a11 <zakeda@mail.ru>
2022-01-19 23:20:41 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println ("Konstantin Gorshkov");
+        System.out.println ("20.08.1981");
+    }
+}
