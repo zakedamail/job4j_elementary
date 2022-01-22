d2108cf4d846be15cf15b17aa93ec9fdc0c57773
a11 <zakeda@mail.ru>
2022-01-22 21:25:22 +0300

    6. Checkstyle [#4123]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class StudentInfo {
     public static void main(String[] args) {
-        System.out.println ("Konstantin Gorshkov");
-        System.out.println ("20.08.1981");
+        System.out.println("Konstantin Gorshkov");
+        System.out.println("20.08.1981");
     }
 }
