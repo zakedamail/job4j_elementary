9f59488540fb9c476aaca7e89734579bfc2c4b10
a11 <zakeda@mail.ru>
2022-01-26 21:26:27 +0300

    4.1. - 1 Разбор первой программы. [#159033 #253214]

---
+++ src/main/java/ru/job4j/Calc.java
@@ -0,0 +1,28 @@
+package ru.job4j;
+
+public class Calc {
+    public static void main(String[] args) {
+        byte mem = 1;
+        System.out.println(mem);
+        short size = 1;
+        System.out.println(size);
+//        int length = 100500;
+//        System.out.println(length);
+        long money = 900500;
+        System.out.println(money);
+        float weight = 1.05F;
+        System.out.println(weight);
+        double distance = 100500.99;
+        System.out.println(distance);
+        boolean exists = true;
+        System.out.println(exists);
+        char gender = 'Y';
+        System.out.println(gender);
+        int age = 18;
+        float length = 78.0332f;
+        boolean baby = true;
+        System.out.println(age);
+        System.out.println(length);
+        System.out.println(baby);
+    }
+}
