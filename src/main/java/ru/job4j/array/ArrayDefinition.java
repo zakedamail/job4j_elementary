822522cdea49c596b31e90df6cf5aa6f3f79e4b9
a11 <zakeda@mail.ru>
2023-06-29 22:11:15 +0300

    6.0.3. Заполнение массива. #226300

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен " + ages.length);
         System.out.println("Размер массива равен " + surnames.length);
         System.out.println("Размер массива равен " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Konstantin Gor";
+        names[1] = "Andrey Gor";
+        names[2] = "Alisa Gor";
+        names[3] = "Georgy Gor";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
